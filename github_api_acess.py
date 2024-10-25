13a7e598797992af344070edb9af3c94b97c1529
rianlucascs <rlucasgit@gmail.com>
2024-10-24 23:38:18 -0300

    github_api_acess update

--- github_api_acess.py
+++ github_api_acess.py
@@ -1,12 +1,15 @@
 import requests
+from pandas import read_csv
+from io import StringIO
+
+# 1. Índices de Segmentos e Setoriais
 
 def get_codigos(setor:str='IDIV') -> list:
     """
     Obtém a lista de códigos de ações de um setor específico a partir de um arquivo TXT no GitHub.
 
     Esta função faz uma requisição a uma URL que contém os códigos das ações de um determinado setor.
-    Se a requisição for bem-sucedida, os códigos são processados e retornados como uma lista. 
-    A função também pode adicionar a extensão '.SA' a cada código, dependendo do parâmetro `sa`.
+    Se a requisição for bem-sucedida, os códigos são processados e retornados como uma lista.
 
     Args:
         setor (str): O nome do setor cujos códigos de ações devem ser obtidos. Os setores disponíveis são:
@@ -30,16 +33,15 @@ def get_codigos(setor:str='IDIV') -> list:
             - 'IBSD': Índice Bovespa Smart Dividendos B3 (Ibov Smart Dividendos B3)
             - 'BDRX': Índice de BDRs Não Patrocinados-GLOBAL (BDRX B3)
             - 'IFIX': Índice de Fundos de Investimentos Imobiliários (IFIX B3)
-        sa (bool): Se True, adiciona a extensão '.SA' a cada código retornado. O padrão é True.
 
     Returns:
-        list: Uma lista contendo os códigos das ações do setor. Se a requisição falhar, a função imprime uma mensagem de erro
-              e retorna None.
+        list: Uma lista contendo os códigos das ações do setor. Se a requisição falhar, a função levanta uma exceção.
     """
     url = f'https://raw.githubusercontent.com/rianlucascs/b3-scraping-project/master/processed_data/1.%20%C3%8Dndices%20de%20Segmentos%20e%20Setoriais/Setores/{setor}/C%C3%B3digos_{setor}.txt'  
-    response = requests.get(url)
-    if response.status_code != 200:
-        raise ValueError(f'Erro ao acessar a página: {response.status_code}, setor: {setor}')
+    try:
+        response = requests.get(url)
+    except requests.exceptions.RequestException as e:
+        raise ValueError(f'Erro ao acessar a página: {e}')
     return [item.strip() for item in response.text.splitlines()]
 
 def get_apresentacao(setor:str) -> str: 
@@ -56,16 +58,38 @@ def get_apresentacao(setor:str) -> str:
         ValueError: Se não for possível acessar a página.
     """
     url = f'https://raw.githubusercontent.com/rianlucascs/b3-scraping-project/master/processed_data/1.%20%C3%8Dndices%20de%20Segmentos%20e%20Setoriais/Setores/{setor}/Apresenta%C3%A7%C3%A3o_{setor}.txt'
-    response = requests.get(url)
-    if response.status_code != 200:
-        raise ValueError(f'Erro ao acessar a página: {response.status_code}, setor: {setor}')
+    try:
+        response = requests.get(url)
+    except requests.exceptions.RequestException as e:
+        raise ValueError(f'Erro ao acessar a página: {e}')
     return response.text
 
 def get_tabela(setor:str):
-    pass
+    """
+    Obtém a tabela de ações de um setor específico a partir de um arquivo CSV no GitHub.
+
+    Esta função faz uma requisição a uma URL que contém a tabela das ações de um determinado setor.
+    Se a requisição for bem-sucedida, a tabela é lida e retornada como um DataFrame.
+
+    Args:
+        setor (str): O nome do setor cujos dados devem ser obtidos.
+
+    Returns:
+        DataFrame: Um DataFrame contendo os dados da tabela. Se a requisição falhar, a função levanta uma exceção.
+    """
+    url = f'https://raw.githubusercontent.com/rianlucascs/b3-scraping-project/master/processed_data/1.%20%C3%8Dndices%20de%20Segmentos%20e%20Setoriais/Setores/{setor}/Tabela_{setor}.csv'
+    try:
+        response = requests.get(url)
+    except requests.exceptions.RequestException as e:
+        raise ValueError(f'Erro ao acessar a página: {e}')
+    return read_csv(StringIO(response.text), delimiter=',')
+
+# 2. Horário de negociação
 
 if __name__ == '__main__':
     
-    setor = 'AGFS'
-    print(get_apresentacao(setor))
-    print(get_codigos(setor))
+    setor = 'IDIV'
+    # print(get_apresentacao(setor))
+    # print(get_codigos(setor))
+
+    print(get_tabela(setor))
