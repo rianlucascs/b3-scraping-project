6487bcee97c757456e01f85c2c017af2dd851ee6
rianlucascs <rlucasgit@gmail.com>
2024-10-26 10:01:35 -0300

    Empreas listadas update driver_page_2

--- scripts/3. Empresas listadas/extract_empresas_listadas_link_acess.py
+++ scripts/3. Empresas listadas/extract_empresas_listadas_link_acess.py
@@ -15,22 +15,16 @@ class Extract:
 
     def __init__(self, path_extracted_data):
         self.path_extracted_data = path_extracted_data
+        self.dir_page = join(self.path_extracted_data, 'paginas')
         self.n_item = 0 
         self.n_page = 1
         pass
     
-    def get_and_save_codigos_page(self, driver:webdriver.Chrome, path, update=False) -> list:
-        """
-        Extrai códigos de seleção da B3.
-
-        :param driver: Instância do WebDriver (ex: webdriver.Chrome).
-        :param path: Caminho para o diretório onde o arquivo será salvo.
-        :param update: Se True, o arquivo existente será atualizado. Caso contrário, será ignorado se já existir.
-        :return: Lista de códigos extraídos, sem duplicatas.
-        """
+    def get_codigos_page(self, driver:webdriver.Chrome) -> list:
         rows = driver.find_elements(By.CLASS_NAME, 'card-title2')
         lista = list(set([row.text for row in rows]))
-        path_file = join(path, 'Códigos_page.txt')
+        # path_file = join(path, 'Códigos_page.txt')
+        return lista
         if not exists(path_file) or update:
             try:
                 with open(path_file, 'w', encoding='utf-8') as file:
@@ -64,25 +58,19 @@ class Extract:
             print("O texto encontrado não pode ser convertido para um inteiro.")
             raise
 
-    def save_url_and_create_dir_codigo(self, url, codigo, path, update=False) -> None:
+    def save_url_codigo(self, url, codigo, path, update=False) -> None:
         """
         Salva uma URL em um arquivo e cria um diretório com o código.
 
-        Esta função verifica se o diretório correspondente ao código já existe. 
-        Se não existir, ele será criado. Em seguida, salva a URL em um arquivo 
-        chamado 'url.txt'. Se o arquivo já existir e o parâmetro `update` for False, 
-        a gravação será ignorada.
+        Salva a URL em um arquivo chamado 'url.txt'. Se o arquivo já existir e o parâmetro 
+        `update` for False, a gravação será ignorada.
 
         :param url: A URL a ser salva.
         :param codigo: O código usado para criar o diretório.
         :param path: Caminho para o diretório onde o novo diretório será criado.
         :param update: Se True, o arquivo existente será atualizado.
         """
-
-        new_dir = join(path, codigo)
-        if not exists(new_dir):
-            makedirs(new_dir)
-        path_file = join(new_dir, f'url_{codigo}.txt')
+        path_file = join(path, f'url_{codigo}.txt')
         if not exists(path_file) or update:
             try:
                 with open(path_file, 'w', encoding='utf-8') as file:
@@ -114,89 +102,79 @@ class Extract:
         except ElementClickInterceptedException:
             print("O clique no botão de próxima página foi interceptado por outro elemento.")
 
+    def get_xpath_numero_pagina(self, driver):
+        xpath_numero_page = driver.find_element(By.CLASS_NAME, f'current')
+        return int(xpath_numero_page.text.split('\n')[1])
 
-    def save_all_codigos(self, path, codigo):
-        with open(join(path, 'all_Códigos'), 'a', encoding='utf-8') as file:
-            file.write(f'{codigo} ')
-
-    def check_codigo(self, path, codigo):
-        if not exists(join(path, 'all_Códigos')):
-            return False
-        with open(join(path, 'all_Códigos'), 'r', encoding='utf-8') as file:
-            if codigo in file.read():
+    #
+    def create_dir_numero_pagina(self, numero_da_pagina):
+        path = join(self.dir_page, f'n_page_{numero_da_pagina}')
+        if not exists(path):
+            makedirs(path)
+        return path
+    
+    def create_dir_codigo(self, numero_da_pagina, codigo):
+        path = join(self.dir_page, f'n_page_{numero_da_pagina}', codigo)
+        if not exists(path):
+            makedirs(path)
+        return path
+    
+    def adjust_numero_pagina(self, driver, numero_pagina_loop):
+        xpath_numero_pagina = self.get_xpath_numero_pagina(driver)
+        if xpath_numero_pagina == numero_pagina_loop:
+            return True
+        sleep(0.3)
+        while True:
+            xpath_numero_pagina = self.get_xpath_numero_pagina(driver)
+            sleep(0.3)
+            if xpath_numero_pagina == numero_pagina_loop:
                 return True
-            return False
+            self.next_page(driver)
 
-    def driver_page(self):
+    def get_codigo_page(self, driver, n_item):
+        return driver.find_element(By.XPATH, f'//*[@id="nav-bloco"]/div/div[{n_item}]/div/div/h5').text
+
+    def acess_page_codigo(self, driver, n_item):
+        element = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, f'//*[@id="nav-bloco"]/div/div[{n_item}]/div/div')))    
+        element.click() 
+
+    def driver_page_2(self):
         url = 'https://sistemaswebb3-listados.b3.com.br/listedCompaniesPage/search?language=pt-br'
-
+        
         options = webdriver.ChromeOptions()
-
         with webdriver.Chrome(options=options) as driver:
             driver.get(url)
-            qtd_paginas = self.get_quantidade_de_paginas(driver)
-            for n_page in list(range(1, qtd_paginas + 1)): # Loop paginas
-
-                print(f'n_page: {n_page}')
-
-                new_dir = join(self.path_extracted_data, 'paginas', f'n_page_{n_page}')
-
-                if not exists(new_dir):
-                    makedirs(new_dir)
-
+            total_paginas = self.get_quantidade_de_paginas(driver)
+            
+            for numero_da_pagina in list(range(1, total_paginas + 1)):
+                self.adjust_numero_pagina(driver, numero_da_pagina)
+                print(f'Número da pagina {numero_da_pagina}')
+                path_dir_page = self.create_dir_numero_pagina(numero_da_pagina)
+                list_codigos = self.get_codigos_page(driver)
                 sleep(3)
+                print(list_codigos)
 
-                codigos_page = self.get_and_save_codigos_page(driver, new_dir, update=True)
-                
-                self.n_item = 0
-                while self.n_item <= len(codigos_page)-1: # Loop itemns da pagina
-                    self.n_item += 1
+                for n_item in list(range(1, len(list_codigos) + 1)):
+                    self.adjust_numero_pagina(driver, numero_da_pagina)
+                    codigo = self.get_codigo_page(driver, n_item)
+                    print(f'Número da pagina {numero_da_pagina} Numero item: {n_item}, Código {codigo}')
                     
-                    if self.n_item == len(codigos_page):
-                        self.n_page += 1
-
-                    n_item = self.n_item
-                    
-                    print(f'n_item: {n_item}')
-                    
-                    codigo = driver.find_element(By.XPATH, f'//*[@id="nav-bloco"]/div/div[{n_item}]/div/div/h5').text
-                    # Xpath que indica a pagina
-                    # //*[@id="listing_pagination"]/pagination-template/ul/li[2]
-                    # //*[@id="listing_pagination"]/pagination-template/ul/li[2]
-                    if not codigo in codigos_page:
-                        self.next_page(driver)
+                    if not exists(join(path_dir_page, codigo)):
                         sleep(3)
-                        self.n_item = self.n_item - 1
-
-                        
-
-                    if not self.check_codigo(join(self.path_extracted_data, 'paginas'), codigo):
-
-                        element = WebDriverWait(driver, 10).until(
-                            EC.element_to_be_clickable((By.XPATH, f'//*[@id="nav-bloco"]/div/div[{n_item}]/div/div')))    
-                        element.click() 
-
+                        self.acess_page_codigo(driver, n_item)
                         sleep(3)
-
-                        url = driver.current_url
-                        
-                        self.save_url_and_create_dir_codigo(url, codigo, new_dir, update=True)
-
+                        path_dir_codigo = self.create_dir_codigo(numero_da_pagina, codigo)
+                        self.save_url_codigo(driver.current_url, codigo, path_dir_codigo, update=True)
                         sleep(3)
-
-                        self.save_all_codigos(join(self.path_extracted_data, 'paginas'), codigo)
                         driver.back()
+                        sleep(3)
 
 
                         
-                
-                
-                
-
-                
             
 
 if __name__ == '__main__':
     
     extract = Extract(config.path_extracted_data)
-    extract.driver_page()
+    extract.driver_page_2()
--- teste.py
+++ teste.py
@@ -2,3 +2,7 @@ with open(r'C:\Users\xxis4\Desktop\b3-scraping-project\extracted_data\3. Empresa
     file = eval(f.read())
 
 print([f for f in file if 'ABCC' in f][0])
+
+list_codigos=  ['ABAP', 'BODY', 'ABRL', 'ABGD', 'NNPY', 'GRDR', 'PTGU', 'ABMX', 'ABCC', 'FCEF', 'AACL', 'AYOS', 'SIVR', 'ABPR', 'ATBC', 'TZRD', 'UQMU', 'ACEC', 'BRAV', 'QVQP']
+for n, item in enumerate(list_codigos, start=1):
+    print(n)
